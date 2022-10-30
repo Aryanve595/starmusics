735bdca53022f374f2ed925582fde379d5839ae7
Aryanve595 <90677181+Aryanve595@users.noreply.github.com>
2022-10-30 07:56:49 +0530

    Update globalban.py

--- GJ516X/plugins/Robot/globalban.py
+++ GJ516X/plugins/Robot/globalban.py
@@ -111,7 +111,7 @@ async def gungabn(client, message: Message, _):
         except Exception:
             pass
     await remove_banned_user(user_id)
-    await message.reply_sticker("CAACAgUAAx0CZp7eIAACaD5jT_BrAvAwadEBDpsd7GVSFeYXPAACdAYAArJscVY0KNBsR3kYDioE")
+     
     await message.reply_text(
         _["gban_9"].format(mention, number_of_chats)
     )
